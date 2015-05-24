c0a703115810ade01fb0adb3b34437ca9bae266d
Andrea Knabe <andrea.knabe@posteo.de>
2015-05-24 22:03:39 +0200

    modified spacing

--- essenv.py
+++ essenv.py
@@ -4,16 +4,17 @@ t = Twitter(
     auth=OAuth('token', 'token_key', 'con_secret', 'con_secret_key'))
 
 text = [
-    'Essen nicht vergessen', 'Und? Mittagessen geplant?',        'Geht jemand mit?', 'Was gibt es heute?', 'Langsam ist es Zeit.',
-    'Was ist nun?', 'An Essen gedacht?', 'Wann gibt es etwas zu essen?',
-    'Ihr dürft nicht vergessen zu essen.', 'Etwas essen wäre jetzt gut.',
-    'Service für die üblichen Verdächtigen: schon geklärt, was es zum Mittag gibt?',
-    'Etwas Essbares dabei?', 'An die üblichen Verdächtigen: Mittagessen geplant?',
-    'Mittag.', 'Gegessen?', 'Dran gedacht?','Was nun?', 'Wie sieht es aus?', 'Mittag?',
-    'Essen - wer noch nicht hat.', 'Schon gegessen?', 'Mittagessen gesichert?',
-    'Und wer der üblichen Verdächtigen wird heute vergessen zu essen?',
-    'Schon über Mittagessen nachgedacht?',
-    'Service für die üblichen Verdächtigen: schon geklärt, was es zu essen gibt?'
+    'Essen nicht vergessen','Und? Mittagessen geplant?','Geht jemand mit?',\n\
+    'Was gibt es heute?','Langsam ist es Zeit.',\n\
+    'Was ist nun?', 'An Essen gedacht?', 'Wann gibt es etwas zu essen?',\n\
+    'Ihr dürft nicht vergessen zu essen.', 'Etwas essen wäre jetzt gut.',\n\
+    'Service für die üblichen Verdächtigen: schon geklärt, was es zum Mittag gibt?',\n\
+    'Etwas Essbares dabei?', 'An die üblichen Verdächtigen: Mittagessen geplant?',\n\
+    'Mittag.', 'Gegessen?', 'Dran gedacht?','Was nun?', 'Wie sieht es aus?', 'Mittag?',\n\
+    'Essen - wer noch nicht hat.', 'Schon gegessen?', 'Mittagessen gesichert?',\n\
+    'Und wer der üblichen Verdächtigen wird heute vergessen zu essen?',\n\
+    'Schon über Mittagessen nachgedacht?',\n\
+    'Service für die üblichen Verdächtigen: schon geklärt, was es zu essen gibt?'\n\
     ]
 from random import choice
 random_index = choice(text)
